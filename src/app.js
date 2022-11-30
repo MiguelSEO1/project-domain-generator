fd40d24e39ae92a957d40fa6a2847858e8c826e8
MiguelSEO1 <miguelsanlucarlebron@gmail.com>
2022-11-30 19:13:11 +0000

    proyecto generador de dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+let domainGenerator = (pronoun, adj, noun) => {
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var r = 0; r < noun.length; r++) {
+        console.log(pronoun[i] + adj[j] + noun[r] + ".com");
+      }
+    }
+  }
 };
+domainGenerator(pronoun, adj, noun);
